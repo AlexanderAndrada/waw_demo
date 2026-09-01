316e04ddb8bca4a0e2be4da336593a76ccad0129
AlexanderAndrada <37665738+AlexanderAndrada@users.noreply.github.com>
2026-09-01 18:22:52 -0300

    Implement custom widget color configuration
    Added custom configuration for widget colors including dark mode options.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-const $navbar = document.querySelector(".main-header")
-const $toggleMenu = document.getElementById("toggleMenu")
+const $navbar = document.querySelector(".main-header");
+const $toggleMenu = document.getElementById("toggleMenu");
 
-$toggleMenu.addEventListener("click", ()=>{
+$toggleMenu.addEventListener("click", () => {
     $navbar.classList.toggle("show")
     $toggleMenu.classList.toggle("show")
 })
 
-const widget = new WAW();
+const customConfiguration = {
+    colors: {
+        mainColor: '#262B5B', // cambia el color del rectangulo del titulo
+        secondColor: '#269FAC', // cambia el color del header y footer del del widget
+        textColor: '#090305', // cambia el color del texto
+        background: '#f4f4f4', // cambia el color del fondo
+        backgroundButton: '#f7f7f7', // cambia el color del fondo de los botones
+        mainColorDarkMode: '#b7234d', // cambia el color del rectangulo del titulo en modo oscuro
+        secondColorDarkMode: '#1c7680', // cambia el color del header y footer del del widget en modo oscuro
+        textColorDarkMode: '#e0e0e0', // cambia el color del texto en modo oscuro
+        backgroundDarkMode: '#2b2b2b', // cambia el color del fondo en modo oscuro
+        backgroundButtonDarkMode: '#333333' // cambia el color del fondo de los botones en modo oscuro
+    }
+}
+
+const widget = new WAW(customConfiguration);
+
 widget.run();
